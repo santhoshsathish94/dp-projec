9d88e992e78f4cb6a60cc4d59651b8c70ea4198e
dilip.repo@gmail.com <dilip.repo@gmail.com@d5aa27f2-e43e-4108-57ec-fdd36bf0c40d>
2014-01-18 20:25:21 +0000

    base  version

---
+++ sm-core/src/main/java/com/salesmanager/core/constants/Constants.java
@@ -0,0 +1,22 @@
+package com.salesmanager.core.constants;
+
+public class Constants {
+	
+	public static final String ALL_REGIONS = "*";
+	public final static String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+	
+	public final static String EMAIL_CONFIG = "EMAIL_CONFIG";
+	public final static String MERCHANT_CONFIG = "MERCHANT_CONFIG";
+	
+	public final static String UNDERSCORE = "_";
+	public final static String SLASH = "/";
+	public final static String TRUE = "true";
+	public final static String FALSE = "false";
+	public final static String OT_ITEM_PRICE_MODULE_CODE = "itemprice";
+	public final static String OT_SUBTOTAL_MODULE_CODE = "subtotal";
+	public final static String OT_TOTAL_MODULE_CODE = "total";
+	public final static String OT_SHIPPING_MODULE_CODE = "shipping";
+	public final static String OT_HANDLING_MODULE_CODE = "handling";
+	public final static String OT_TAX_MODULE_CODE = "tax";
+
+}
