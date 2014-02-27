111d364c34ebc53ef46ca8dabf88829c2a67f70b
abhishekverma.av@gmail.com <abhishekverma.av@gmail.com@d5aa27f2-e43e-4108-57ec-fdd36bf0c40d>
2014-02-27 00:04:48 +0000

    Changes:
    - Added Company Controller
    - Added Company Service
    - Added Company Dao
    - Added Jsp Related to Company
    - Modify tiles-admin.xml, menu.json, shopizer.properties, message.properties

---
+++ trunk/OIMS5/sm-core/src/main/java/com/salesmanager/core/business/company/service/CompanyServiceImpl.java
@@ -0,0 +1,82 @@
+package com.salesmanager.core.business.company.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.salesmanager.core.business.company.dao.CompanyDao;
+import com.salesmanager.core.business.company.model.AccountingPeriod;
+import com.salesmanager.core.business.company.model.Company;
+import com.salesmanager.core.business.generic.exception.ServiceException;
+import com.salesmanager.core.business.generic.service.SalesManagerEntityServiceImpl;
+
+@Service("companyService")
+public class CompanyServiceImpl extends SalesManagerEntityServiceImpl<Integer, Company> implements CompanyService {
+
+	private CompanyDao companyDao;
+	
+	@Autowired
+	public CompanyServiceImpl(CompanyDao companyDao) {
+		super(companyDao);
+		this.companyDao = companyDao;
+	}
+
+	@Override
+	public Company getCompany(Integer companyId) throws ServiceException {
+		return companyDao.getCompany(companyId);
+	}
+
+	
+	
+	@Override
+	public void saveOrUpdate(Company company) throws ServiceException {
+		
+		if(company.getId()==null) {
+			super.save(company);
+		} else {
+			super.update(company);
+		}
+	}
+
+	@Override
+	public Company getByCode(String code) throws ServiceException {
+		// TODO Auto-generated method stub
+		return companyDao.getCompanyDetails(code);
+	}
+
+	@Override
+	public AccountingPeriod getByAccountingPeriodId(Long id)
+			throws ServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AccountingPeriod> listAccountingPeriod()
+			throws ServiceException {
+		return companyDao.listAccountingPeriod();
+	}
+
+	@Override
+	public List<AccountingPeriod> listAccountingPeriodByCompany(Company company)
+			throws ServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveOrUpdate(Company company, AccountingPeriod accountingPeriod)
+			throws ServiceException {
+		
+		if(accountingPeriod.getId() == null) {
+			company.getAccountingPeriod().add(accountingPeriod);
+			accountingPeriod.setCompany(company);
+		} else {
+			
+		}
+		
+		super.update(company);
+	}
+
+}
